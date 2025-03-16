bb111544271201a17208e60c4249db15b97b519f
kevin-joshua <kevinajijoshua@gmail.com>
2025-03-16 17:14:21 +0530

    push frontend files

--- backend/middleware/role.middleware.js
+++
@@ -1,6 +0,0 @@
-export const roleMiddleware = async(req, res, next) => {
-  if(!req.user || req.user.role !== "Admin"){
-    return res.status(403).json({ message: "Access denied" });
-  }
-  next();
-}
--- backend/utils/generateToken.js
+++
@@ -1,9 +0,0 @@
-import jwt from 'jsonwebtoken';
-import 'dotenv/config'
-
-const JWT_SECRET = process.env.JWT_SECRET;
-export const generateToken = async(id) => {
-   return jwt.sign({id}, JWT_SECRET, {expiresIn: "12h"});
-}
-
-
